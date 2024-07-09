c3dc8825111c4bf6322cfb63da5d213bea7b83ed
Daniel Igor <danieligorxd@gmail.com>
2024-07-09 19:30:22 -0300

    Construção do text área

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatBadgeModule} from '@angular/material/badge';
-
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +15,8 @@ import {MatBadgeModule} from '@angular/material/badge';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
